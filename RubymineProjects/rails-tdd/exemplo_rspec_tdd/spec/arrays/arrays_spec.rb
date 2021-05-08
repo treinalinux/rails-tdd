a65d8a7d2ad2e64b6f72f61d032d19634c13d275
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-08 18:05:26 -0300

    Working define_negated_matcher

--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/arrays/arrays_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/arrays/arrays_spec.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1, 2, 3, 4]), 'Arrays' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2, 1)
   end
+
+  it { expect(subject).to exclude(5) }
 
   it '#match array' do
     expect(subject).to match_array([1, 2, 3, 4])
--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/be_within/be_within_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/be_within/be_within_spec.rb
@@ -1,5 +1,8 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 describe 'be_within' do
   # within 11.5 - 12.5
   # this is delta 0.5 for up or down, delta work float
-  it { expect(12.5).to be_within(0.5).of(12) }
+  it { expect(11.5).to be_within(0.5).of(12) }
+  it { expect(11.4).to be_not_within(0.5).of(12) }
 end
