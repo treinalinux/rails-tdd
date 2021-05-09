a9f6a0cb215b0da6d67983e796ccf2b4a7321810
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-09 08:11:57 -0300

    Methods Stubs

--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'student'
 require 'course'
 
@@ -12,5 +14,39 @@ describe 'Stub' do
     course_finished = student.has_finished?(course)
 
     expect(course_finished).to be_truthy
+  end
+
+  it 'Dynamic Arguments' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      case arg
+      when :hello
+        'Hello!'
+      when :hi
+        'Hi!!'
+      end
+    end
+
+    expect(student.foo(:hello)).to eq('Hello!')
+    expect(student.foo(:hi)).to eq('Hi!!')
+  end
+
+  it 'Any instance of class' do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it 'Errors' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect{ student.bar }.to raise_error(RuntimeError)
   end
 end
