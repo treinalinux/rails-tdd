b28c1a9e875819258eca9ce6316114f48105ec95
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-09 07:44:44 -0300

    Stubs

---
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/lib/student.rb
@@ -0,0 +1,7 @@
+class Student
+  attr_accessor :namei, :email
+
+  def has_finished?(course)
+    ### true /false
+  end
+end
---
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -0,0 +1,16 @@
+require 'student'
+require 'course'
+
+describe 'Stub' do
+  # Stub ..
+  it '#has_finished?' do
+    student = Student.new
+    course = Course.new
+
+    allow(student).to receive(:has_finished?).with(an_instance_of(Course)).and_return(true)
+
+    course_finished = student.has_finished?(course)
+
+    expect(course_finished).to be_truthy
+  end
+end
