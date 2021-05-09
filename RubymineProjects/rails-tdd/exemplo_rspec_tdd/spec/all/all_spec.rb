6d8fa8df56456c348238b723d954de50588c150e
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-08 21:07:02 -0300

    Tag Filters for run only tests desired

--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/all/all_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/all/all_spec.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
-describe 'all' do
+# you can use key: true or key: value
+# can use collection: true for example of key: true, but can you to omit 'true'
+# can use type: 'collection' for example of key: value
+# go call rspec . -t collection for key: true
+# go call rspec . -t type:collection for key: value
+#
+# is possible marked tests how to deny on .rspec file:
+# --tag ~slow
+# need you added on it an ... :slow do..
+# ...
+# Run options: include {:collection=>true}
+describe 'all', collection: true do
   it { expect([1, 7, 9]).to all((be_odd).and(be_an(Integer))) }
   it { expect(%w[ruby rails]).to all(be_a(String).and(include('r'))) }
 end
