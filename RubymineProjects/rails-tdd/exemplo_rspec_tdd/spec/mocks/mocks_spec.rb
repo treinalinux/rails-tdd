65a1bf695168260eec5074efbe4ff74199835edf
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-09 09:58:59 -0300

    Mocks Expectations

--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
@@ -21,4 +21,23 @@ describe 'Mocks' do
     # phase exercises
     student.bar
   end
+
+  it '#args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  it '#repetition' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it '#return' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    puts student.foo(123)
+  end
 end
