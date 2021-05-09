adaf6b21ca94458ec051ef01977160303ca1822b
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-09 10:04:38 -0300

    as_null_object

--- RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
+++ RubymineProjects/rails-tdd/exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -6,4 +6,12 @@ describe 'Test Double' do
     user.name
     user.password
   end
+
+  it 'as_null_object' do
+    # messages of error will null
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name: 'Alan', password: 'secret')
+    user.name
+    user.password
+  end
 end
